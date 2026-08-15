583fce02d4255d0d2fc6e70c6d0062b9b1151edc
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-08-15 22:21:37 +0800

    feat: initialize Fiber app and set up createTicket route

--- server/main.go
+++ server/main.go
@@ -1,8 +1,23 @@
 package main
 
-import "ticketing-it-app/server/config"
+import (
+	"ticketing-it-app/server/config"
+	"ticketing-it-app/server/handler"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
 	// Panggil fungsi koneksi database yang ada di folder config
 	config.ConnectDatabase()
+
+	// Inisiasi FIber map
+	app := fiber.New()
+
+	// Buat routing group
+	api := app.Group("/api")
+	api.Post("/createTicket", handler.CreateTicket)
+
+	// Listen di port 8080
+	app.Listen(":8080")
 }
