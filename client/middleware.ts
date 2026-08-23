0fea27bd544e77b937d5542a50b13ea337cb30c6
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-08-23 20:38:59 +0800

    feat: FE | add middleware for authentication and redirection based on user login status

---
+++ client/middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export function middleware(request: NextRequest) {
+    const token = request.cookies.get('token')?.value;
+    const { pathname } = request.nextUrl;
+
+    // Jika belum login tapi ingin mengakses halaman dashboard, maka akan dialihkan ke halaman login
+    if (!token && pathname.startsWith('/dashboard')) {
+        return NextResponse.redirect(new URL('/login', request.url))
+    }
+
+    // sebaliknya, jika sudah login tapi ingin mengakses halaman login atau register maka akan dialihkan ke halaman dashboard
+    if (token && (pathname === '/login' || pathname === '/regiter')) {
+        return NextResponse.redirect(new URL('/dasboard', request.url))
+    }
+
+    return NextResponse.next()
+}
+
+
+export const config = {
+    matcher: ['/dashboard/:path*', '/login', '/register'],
+}
