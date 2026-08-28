c70aeaefe7873329badd9f2fac8b1a697da9bb8c
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-08-28 22:27:14 +0800

    fix: BE | add JSON tags and GORM annotations to BaseModel fields

--- server/models/base.go
+++ server/models/base.go
@@ -7,10 +7,10 @@ import (
 )
 
 type BaseModel struct {
-	ID        uint
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	DeletedAt gorm.DeletedAt
-	CreatedBy uint
-	UpdatedBy uint
+	ID        uint           `json:"id" gorm:"primaryKey"`
+	CreatedAt time.Time      `json:"created_at"`
+	UpdatedAt time.Time      `json:"updated_at"`
+	DeletedAt gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
+	CreatedBy uint           `json:"created_by"`
+	UpdatedBy uint           `json:"updated_by"`
 }
