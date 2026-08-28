8e6b5b5bea587dd924eaa7e60a72f1750a238952
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-08-28 22:29:14 +0800

    fix: BE | add JSON tags to Email and Role fields in User model for improved serialization

--- server/models/user.go
+++ server/models/user.go
@@ -12,7 +12,7 @@ type User struct {
 	BaseModel
 
 	Name     string
-	Email    string `gorm:"unique;not null"`
+	Email    string `json:"email" gorm:"unique;not null"`
 	Password string `gorm:"not null" json:"-"` //tag json:"-" adalah untuk TIDAK pernah terkirim di respon JSON
-	Role     Role   `gorm:"type:varchar(20);default:'Pelapor'"`
+	Role     Role   `json:"role" gorm:"type:varchar(20);default:'Pelapor'"`
 }
