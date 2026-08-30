cce494bf427eaf649b7081c18a6d6fd84d26b81a
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-08-30 21:17:17 +0800

    feat: BE | update BaseModel to use pointers for UpdatedAt, UpdatedBy, and DeletedBy fields

--- server/models/base.go
+++ server/models/base.go
@@ -9,8 +9,9 @@ import (
 type BaseModel struct {
 	ID        uint           `json:"id" gorm:"primaryKey"`
 	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
+	UpdatedAt *time.Time     `json:"updated_at,omitempty" gorm:"autoUpdateTime:false"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
 	CreatedBy uint           `json:"created_by"`
-	UpdatedBy uint           `json:"updated_by"`
+	UpdatedBy *uint          `json:"updated_by,omitempty"`
+	DeletedBy *uint          `json:"deleted_by,omitempty"`
 }
