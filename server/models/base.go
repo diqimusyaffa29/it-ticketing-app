66e97019a3b5a1ad58622c7abf88e3e3d9c8928d
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-09-02 09:26:14 +0800

    feat: BE | change ID and user fields to use UUID for improved uniqueness

--- server/models/base.go
+++ server/models/base.go
@@ -3,15 +3,26 @@ package models
 import (
 	"time"
 
+	"github.com/google/uuid"
+
 	"gorm.io/gorm"
 )
 
 type BaseModel struct {
-	ID        uint           `json:"id" gorm:"primaryKey"`
+	ID        uuid.UUID      `json:"id" gorm:"type:uuid;primaryKey"`
 	CreatedAt time.Time      `json:"created_at"`
 	UpdatedAt *time.Time     `json:"updated_at,omitempty" gorm:"autoUpdateTime:false"`
 	DeletedAt gorm.DeletedAt `json:"deleted_at,omitempty" gorm:"index"`
-	CreatedBy uint           `json:"created_by"`
-	UpdatedBy *uint          `json:"updated_by,omitempty"`
-	DeletedBy *uint          `json:"deleted_by,omitempty"`
+	CreatedBy uuid.UUID      `json:"created_by"`
+	UpdatedBy *uuid.UUID     `json:"updated_by,omitempty"`
+	DeletedBy *uuid.UUID     `json:"deleted_by,omitempty"`
+}
+
+// Hook ini otomatis generate UUID sebelum row di-insert,
+// jadi kamu TIDAK perlu setting ID manual di handler manapun
+func (b *BaseModel) BeforeCreate(tx *gorm.DB) error {
+	if b.ID == uuid.Nil {
+		b.ID = uuid.New()
+	}
+	return nil
 }
