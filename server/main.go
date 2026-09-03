8a822f2087a8cbac0962d315b71325c9119e6df0
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-09-03 21:22:00 +0800

    feat: BE | add endpoint to retrieve only active tickets

--- server/main.go
+++ server/main.go
@@ -41,6 +41,7 @@ func main() {
 	// Pelapor, Teknisi, & Admin BISA membuat dan melihat tiket
 	tickets.Post("/", handler.CreateTicket)
 	tickets.Get("/", handler.GetTickets)
+	tickets.Get("/active", handler.GetOnlyActiveTickets)
 	tickets.Get("/:id", handler.GetTicketById)
 
 	// HANYA Admin & Teknisi yang BISA mengubah tiket (Update Status/Assignee)
