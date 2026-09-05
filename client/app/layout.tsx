d3e56866b289a6c4835a5ece10f42253b257c2e6
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-09-05 23:07:53 +0800

    feat: FE | wrap children in TooltipProvider for enhanced tooltip functionality

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import { TooltipProvider } from "@/components/ui/tooltip";
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -19,7 +20,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
       lang="en"
       className={`${poppins.className} ${poppins.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col">
+        <TooltipProvider>
+          {children}
+        </TooltipProvider>
+      </body>
     </html>
   );
 }
