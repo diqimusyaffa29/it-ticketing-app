d88708bc66a2abab80cf3ea06110acaf6bbc7796
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-09-06 18:09:53 +0800

    feat: FE | enhance axios interceptor for improved token handling and error redirection

--- client/lib/axios.ts
+++ client/lib/axios.ts
@@ -1,21 +1,37 @@
 import axios from "axios";
 import { getCookie } from "cookies-next";
+import { removeAuthToken } from "./auth";
+import { redirect } from "next/navigation";
 
 const api = axios.create({
     baseURL: 'http://localhost:8080/api',
-    headers:{
+    headers: {
         'Content-Type': 'application/json',
     },
 });
 
 // interceptor: sisipkan JWT Token dari Cookies ke setiap request
-api.interceptors.request.use((config)=>{
+api.interceptors.request.use((config) => {
     const token = getCookie('token');
-    if(token){
+    if (token) {
         config.headers.Authorization = `Bearer ${token}`
     }
     return config;
 })
+
+// interceptop untuk tangani token invalid/expired secara global 
+api.interceptors.response.use(
+    (response) => response,
+    (error) => {
+        if (error.response?.status === 401) {
+            removeAuthToken()
+            if (typeof window !== 'undefined') {
+                redirect('/login')
+            }
+        }
+        return Promise.reject(error)
+    }
+)
 
 
 export default api
