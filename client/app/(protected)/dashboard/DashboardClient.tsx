d24555672e48047dd71b692d54d22727e08b7aab
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-09-10 20:16:51 +0800

    refactor: Refactred Dashboard client card

--- client/app/(protected)/dashboard/DashboardClient.tsx
+++ client/app/(protected)/dashboard/DashboardClient.tsx
@@ -1,35 +1,8 @@
 import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import React from 'react'
 
 const DashboardClient = () => {
   return (
     <div className="grid grid-cols-3 space-x-4">
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-          <CardAction>Card Action</CardAction>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
-      <Card>
-        <CardHeader>
-          <CardTitle>Card Title</CardTitle>
-          <CardDescription>Card Description</CardDescription>
-          <CardAction>Card Action</CardAction>
-        </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
-        </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
-      </Card>
       <Card>
         <CardHeader>
           <CardTitle>Card Title</CardTitle>
