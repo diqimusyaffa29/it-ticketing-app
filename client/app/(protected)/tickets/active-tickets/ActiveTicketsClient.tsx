ec58eade8fbbce86ea19b570e1598515d3b49ce7
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-09-11 22:54:42 +0800

    refactor: FE | remove unused Badge import in ActiveTicketsClient

--- client/app/(protected)/tickets/active-tickets/ActiveTicketsClient.tsx
+++ client/app/(protected)/tickets/active-tickets/ActiveTicketsClient.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
