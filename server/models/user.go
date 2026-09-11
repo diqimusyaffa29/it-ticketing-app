a54a97f4f217e7d70627be14f70c2ea2553ab42b
diqimusyaffa29 <diqimusyaffa4@gmail.com>
2026-09-11 22:30:09 +0800

    refactor: BE | correct User struct to use Username field instead of Email

--- server/models/user.go
+++ server/models/user.go
@@ -12,7 +12,7 @@ type User struct {
 	BaseModel
 
 	Name     string
-	Email    string `json:"email" gorm:"unique;not null"`
+	Username string `json:"username" gorm:"unique;not null"`
 	Password string `gorm:"not null" json:"-"` //tag json:"-" adalah untuk TIDAK pernah terkirim di respon JSON
 	Role     Role   `json:"role" gorm:"type:varchar(20);default:'Pelapor'"`
 }
